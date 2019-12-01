066fb949fc08df24ca2b7620102e4a5147b17887
allem40306 <allem40306@gmail.com>
2019-12-01 21:56:37 +0800

    add tests

--- lunch/stores/tests.py
+++ lunch/stores/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .models import Store, MenuItem
 
 # Create your tests here.
+class HomeViewTests(TestCase):
+    def test_home_view(self):
+        response = self.client.get('/')
+        # self.assertEqual(response.status_code, 200)
+        # self.assertTemplateUsed(response, 'home.html')
+
+class StoreViewTests(TestCase):
+
+    def setUp(self):
+        Store.objects.create(name='AC', notes='就是爽')
+        AC = Store.objects.create(name='AC')
+        MenuItem.objects.create(store=AC, name='001', price=100)
+
+    def tearDown(self):
+        Store.objects.all().delete()
+        
+    def test_list_view(self):
+        r = self.client.get('/store/')
+        self.assertContains(
+            r, '<a class="navbar-brand" href="/">午餐系統</a>',
+            html=True,
+        )
+        self.assertContains(r, '<a href="/store/1/">AC</a>', html=True)
+        self.assertContains(r, '就是爽')
+    
+    # def test_detail_view(self):
+    #     response = self.client.get('/store/1/')
+    #     self.assertContains(
+    #         response, '<tr><td>001</td><td>100</td></tr>',
+    #         html=True,
+    #     )
