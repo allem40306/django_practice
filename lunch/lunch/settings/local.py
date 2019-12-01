310d60f55d1fa4451f0b7f69a74167626f19962a
allem40306 <allem40306@gmail.com>
2019-12-01 15:13:57 +0800

    set db mysql

--- lunch/lunch/settings/local.py
+++ lunch/lunch/settings/local.py
@@ -12,10 +12,24 @@ TEMPLATE_DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_mysql',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'POST': 3306,
+          'OPTIONS': {
+              "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",                                                              
+          }
+
     }
 }
-
