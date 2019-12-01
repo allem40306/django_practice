d37bf9dfe0d75783e0aa32d3c236576dc07b2eed
allem40306 <allem40306@gmail.com>
2019-12-01 23:09:25 +0800

    move stores' template to /stores

--- lunch/stores/models.py
+++ lunch/stores/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,7 +10,9 @@ class Store(models.Model):
 
     def __str__(self):
         return self.name
-
+    
+    def get_absolute_url(self):
+        return reverse('store_detail', kwargs={'pk': self.pk})
 
 class MenuItem(models.Model):
 
--- lunch/stores/views.py
+++ lunch/stores/views.py
@@ -6,11 +6,11 @@ from .models import Store
 
 def store_list(request):
     stores = Store.objects.all()
-    return render(request, 'store_list.html', {'stores': stores})
+    return render(request, 'stores/store_list.html', {'stores': stores})
 
 def store_detail(request, pk):
     try:
         store = Store.objects.get(pk=pk)
     except Store.DoesNotExist:
         raise Http404
-    return render(request, 'store_detail.html', {'store': store})
+    return render(request, 'stores/store_detail.html', {'store': store})
